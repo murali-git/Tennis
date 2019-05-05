39c746875698ae0321ea6d824f72a1ac8908b064
Murali Sankar <murali@Muralis-MacBook-Pro.local>
2019-05-05 18:56:42 +0200

    Implemented test_ShouldReturnLoveAll_WhenTennisGameIsInitialized

--- TennisTests/TennisGameTests.swift
+++ TennisTests/TennisGameTests.swift
@@ -4,13 +4,17 @@ import XCTest
 class TennisGameTests: XCTestCase {
     
     func test_ShouldReturnLoveAll_WhenTennisGameIsInitialized() {
-        let firstPlayer = buildPlayer("FirstPlayer")
-        let secondPlayer = buildPlayer("SecondPlayer")
-        let tennisGame = TennisGame.init(firstPlayer, secondPlayer)
+        let tennisGame = buildTennisGame()
         
         XCTAssertEqual("LoveAll",tennisGame.getPlayersScore())
     }
     
+    private func buildTennisGame() -> TennisGame {
+        let firstPlayer = buildPlayer("FirstPlayer")
+        let secondPlayer = buildPlayer("SecondPlayer")
+
+        return TennisGame.init(firstPlayer, secondPlayer)
+    }
     
     private func buildPlayer(_ playerName: String) -> Player {
         return try! Player.init(playerName)
