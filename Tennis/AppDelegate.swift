445da6947a54eb332f57906cce114710fbbf0501
Murali Sankar <murali@Muralis-MacBook-Pro.local>
2019-05-05 23:49:16 +0200

    Added Tennis VC and created as a initial screen in the app

--- Tennis/AppDelegate.swift
+++ Tennis/AppDelegate.swift
@@ -4,8 +4,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var navController: UINavigationController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame:UIScreen.main.bounds)
+        let tennisViewController = TennisViewController(nibName: "TennisViewController", bundle: nil)
+        navController = UINavigationController(rootViewController: tennisViewController)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
---
+++ Tennis/ViewController/TennisViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class TennisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
