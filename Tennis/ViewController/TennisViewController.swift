dbda0ea360626276edfad9927accb29bb2f2d751
Murali Sankar <murali@muralis-mbp.home>
2019-05-06 00:27:45 +0200

    Created a simple view and called presenter class to execute it

--- Tennis/ViewController/TennisViewController.swift
+++ Tennis/ViewController/TennisViewController.swift
@@ -1,8 +1,38 @@
 import UIKit
 
 class TennisViewController: UIViewController {
-
+    private var tennisGamePresenter: TennisGamePresenter!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationItem.title = "Tennis"
+        buildTennisGame()
+    }
+    
+    @IBAction func playerOneWins(_ sender: Any) {
+        tennisGamePresenter.playerWinsThePoint(.FirstPlayer)
+        updateScore(tennisGamePresenter.getPlayersScore())
+    }
+    
+    @IBAction func playerTwoWins(_ sender: Any) {
+        tennisGamePresenter.playerWinsThePoint(.SecondPlayer)
+        updateScore(tennisGamePresenter.getPlayersScore())
+    }
+    
+    private func buildTennisGame() {
+        do {
+            let firstPlayer = try Player("FirstPlayer")
+            let secondPlayer = try Player("SecondPlayer")
+            
+            self.tennisGamePresenter = TennisGamePresenter.init(firstPlayer, secondPlayer)
+        }
+        catch {
+            updateScore("Exception in creating Tennis Presnter Class")
+        }
+    }
+    
+    private func updateScore(_ score: String) {
+        scoreLabel.text = score
     }
 }
